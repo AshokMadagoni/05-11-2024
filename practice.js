c79793364323018b387be1de6decbf123991f34f
AshokMadagoni <madagoniashok1@gmail.com>
2024-11-14 15:49:03 +0530

    added

---
+++ practice.js
@@ -0,0 +1,25 @@
+// bom methods
+// 1.location
+location.href="practice.html"
+location.assign="practice.html"
+
+// history
+
+history.back()
+history.forward()
+history.go()
+
+// popups
+
+alert("this is alert popup")
+prompt("this is promt popup")
+confirm()
+close() & open()
+console.warn("be carefull")
+window.open("practice.html")
+
+// screen
+
+screen.innerHeight
+screen.innerWidth
+screen.pixelDepth
